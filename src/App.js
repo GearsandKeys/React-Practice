25cc10799d83de43c399dc8a3d4d1a252b71888a
Nathan Gearke <n.gearke@gmail.com>
2021-03-12 22:19:55 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+function App() {
+  //here we cab delcare variables and wirte javascript
+
+  const x = true;
+  const name = "Nathan"
+
+  return (
+    <div className="Container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+//Just typed rafce and the snippet filled this in
+//import React from 'react' //this was here, but isn't needed anymore
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+export default Header
+
