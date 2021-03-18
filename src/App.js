2826a19c86bba8c27e3e42b2418ba492dcd828d0
Nathan Gearke <n.gearke@gmail.com>
2021-03-17 20:06:19 -0500

    Added a delete function, learned about global components, and using awesome font

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-function App() {
-  //here we cab declare variables and wirte javascript
+
+const App = () => {
+  //here we cab declare variables and write javascript
   //const x = true;
   //const name = "Nathan"
+  const [tasks, setTasks] = useState([ //tasks = default state below
+    //app level state
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    },
+])
 
+//Delete Task
+const deleteTask = (id) => {
+  //console.log('delete', id) //great way to test
+  setTasks(tasks.filter((tasks) => tasks.id !== id))
+}
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} 
+      onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,39 +1,13 @@
-import { useState } from 'react'
-//here's how to loop through
+//don't forget, useState is immutable
+import Task from './Task'
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState( //default state below
-        [
-            {
-                id: 1,
-                text: 'Doctors Appointment',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 2,
-                text: 'Meeting at School',
-                day: 'Feb 6th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 3,
-                text: 'Food Shopping',
-                day: 'Feb 5th at 2:30pm',
-                reminder: false,
-            },
-        ]
-    )
-
+const Tasks = ({tasks, onDelete}) => { //Could also do props.tasks
     return (
         <>
-            {tasks.map((task) => (
-            <div key={task.id} style={{color: "red"}}>
-                <h2>{task.day}</h2>
-                <h3>{task.text}</h3>
-                <br></br>
-            </div>
-             //loop through and make a h3 header with the text for each
+            {tasks.map((task) => ( //Here's how to loop through
+                <Task key={task.id} task={task} 
+                onDelete={onDelete} />
+             //Loop through and make a h3 header with the text for each
             ))}
         </>
     )
